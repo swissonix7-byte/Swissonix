7d38bf5d1ff8587d0eccd63217f377eb360e68a3
Gooclaim <gooclaim.health@gmail.com>
2025-10-04 11:48:06 +0530

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -73,7 +73,7 @@ export default function Hero() {
               </div>
               <div className="flex items-center space-x-2 text-gray-400 whitespace-nowrap">
                 <div className="w-8 h-8 bg-gray-200 rounded-lg flex items-center justify-center">
-                  <img src="https://media.licdn.com/dms/image/v2/D4E0BAQEieRREr7JctQ/company-logo_200_200/company-logo_200_200/0/1730104690449/111pg_logo?e=1762387200&v=beta&t=WQUa9cDUdPV829jTIWrrZe-l80BG141Szye3RHuSdPU" alt="" />
+                  <img src="https://res.cloudinary.com/dvgoquoqv/image/upload/v1759518191/images_nrwjqm.png" alt="" />
                 </div>
                 <span className="font-medium">MTV</span>
               </div>
